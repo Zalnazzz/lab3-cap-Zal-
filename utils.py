d09f3e1b36caafdb49a54a40ad519378f6ca7a4e
name <my@example.com>
2024-02-13 21:43:54 +0300

    prost

--- utils.py
+++ utils.py
@@ -3,3 +3,12 @@ def Fact2(n):
         return 1
     else:
         return n * Fact(n - 1)
+
+from math import ceil
+def prost2(n):
+    i = 2
+    while i <= ceil(n ** (1 / 2)):
+        if n % i == 0:
+            return "не просте"
+        i = i + 1
+    return "просте"
