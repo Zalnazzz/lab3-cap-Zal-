6b8ad1d05c6d81362a2ecdee6f35550b7ecdcc8d
HudzenkoVlad <hudzenkovladislav@knu.ua>
2024-02-15 20:38:06 +0200

    Zal2Commit

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
-from utils import Fact2, prost2, FiveDegree
+from utils import Fact2, prost2, FiveDegree2
 a = int(input())
-print(Fact2(a), prost2(a), FiveDegree(a))
+print(Fact2(a), prost2(a), FiveDegree2(a))
